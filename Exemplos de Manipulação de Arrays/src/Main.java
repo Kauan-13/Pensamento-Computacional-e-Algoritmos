9d1c96d81b1d0a5ee8d42e72cbd6677a51990618
Kauan Freitas <94935829+Kauan-13@users.noreply.github.com>
2024-04-30 15:39:07 -0300

    Add files via upload
    adicionando arquivos de exemplos de manipulação de arrays

---
+++ Exemplos de Manipulação de Arrays/src/Main.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//--------Exemplos de manipulação de Arrays--------//
+
+//        Exemplo 1. Solicitação de alunos e notas e verificando quem tem a maior nota entre eles.
+
+//        Scanner scanner = new Scanner(System.in);
+//        String[] nomes = new String[2];
+//        double[] notas = new double[2];
+//        double maiorNota = 0;
+//        String nome = "";
+//
+//        for(int i = 0; nomes[nomes.length - 1] == null; i++){
+//            System.out.println("Digite um Nome");
+//            nomes[i] = scanner.next();
+//            System.out.println("digite a nota do(a): " + nomes[i]);
+//            notas[i] = scanner.nextDouble();
+//        }
+//
+//        for(int i = 0; i < nomes.length; i++){
+//            if(notas[i] > maiorNota){
+//                maiorNota = notas[i];
+//                nome = nomes[i];
+//            }
+//        }
+//
+//        System.out.println(nome + " tirou a maior nota, com a nota igual a " + maiorNota);
+
+
+
+//        Exemplo 2, Invertendo um array
+
+//        int[] inteiros = {1,2,3,4,5,6,7,8,9,10};
+//        int[] inversos = new int[inteiros.length];
+//
+//        for (int i = (inteiros.length - 1); i >= 0; i--) {
+//            inversos[(inteiros.length - 1 - i)] = inteiros[i];
+//        }
+//
+//        for (int i = 0; i < inversos.length ; i++) {
+//            System.out.println(inversos[i]);
+//        }
+
+
+//        Exemplo 3, Unindo dois arrays
+
+//        int[] menores = {0,1,2,3,4,5};
+//        int[] maiores = {6,7,8,9};
+//        int[] inteiros = new int[(menores.length + maiores.length)];
+//
+//        for (int i = 0; i < menores.length; i++) {
+//            inteiros[i] = menores[i];
+//        }
+//
+//        for (int i = 0; i < maiores.length; i++) {
+//            inteiros[menores.length + i] = maiores[i];
+//        }
+//
+//        for (int i = 0; i < inteiros.length; i++) {
+//            System.out.println(inteiros[i]);
+//        }
+    }
+}
